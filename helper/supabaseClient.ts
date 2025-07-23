7c6f8be15bcddb264b24d5001d4132f008151420
Vishesh Sharma <visheshsharma01232@gmail.com>
2025-07-23 22:39:17 +0530

    final commit

--- helper/supabaseClient.ts
+++ helper/supabaseClient.ts
@@ -1,7 +1,6 @@
+import { SUPABASE_ANNON_KEY, SUPABASE_URL } from '@env';
 import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://jxkbupsmbujlotleyfwt.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4a2J1cHNtYnVqbG90bGV5Znd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxNjQ4NTcsImV4cCI6MjA2ODc0MDg1N30.MWZMB13ljEM9145UwV12l3CElBy-ePspX0uXHHAiaJQ";
+const supabaseUrl = SUPABASE_URL;
+const supabaseAnonKey = SUPABASE_ANNON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
